67421771beba6df3cd1bc517f774d06f2218efef
Maksimova Kristina <kris.maks.t.7@gmail.ru>
2023-11-22 14:28:54 +0300

    Initial Commit

---
+++ FortuneByMemes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FortuneByMemes
+//
+//  Created by Кристина Максимова on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
